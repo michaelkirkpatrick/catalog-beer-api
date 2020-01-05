1a6917e9054144eb5bfb6c18a6bd96aaca8bc51a
Michael Kirkpatrick <michael@mekstudios.com>
2020-01-05 11:29:03 -0800

    Updated Brewer.class.php - reponseCode()

--- classes/Brewer.class.php
+++ classes/Brewer.class.php
@@ -18,6 +18,7 @@ class Brewer {
 	public $errorMsg = '';
 	public $validState = array('name'=>'', 'url'=>'', 'description'=>'', 'short_description'=>'', 'facebook_url'=>'', 'twitter_url'=>'', 'instagram_url'=>'');
 	public $validMsg = array('name'=>'', 'url'=>'', 'description'=>'', 'short_description'=>'', 'facebook_url'=>'', 'twitter_url'=>'', 'instagram_url'=>'');
+	public $responseCode = 200;
 	
 	// Add Brewer
 	public function add($name, $description, $shortDescription, $url, $facebookURL, $twitterURL, $instagramURL, $userID){		
@@ -46,6 +47,7 @@ class Brewer {
 			// UUID Generation Error
 			$this->error = true;
 			$this->errorMsg = $uuid->errorMsg;
+			$this->responseCode = $uuid->responseCode;
 		}
 		
 		if(!$this->error){
@@ -103,11 +105,16 @@ class Brewer {
 					// Query Error
 					$this->error = true;
 					$this->errorMsg = $db->errorMsg;
+					$this->responseCode = $db->responseCode;
 				}
+				
+				// Close Database Connection
+				$db->close();
 			}else{
 				// User Validation Error
 				$this->error = true;
 				$this->errorMsg = $users->errorMsg;
+				$this->responseCode = $users->responseCode;
 			}
 		}
 	}
@@ -121,6 +128,7 @@ class Brewer {
 			// Invalid Brewer ID
 			$this->error = true;
 			$this->errorMsg = 'Sorry, the brewerID you provided appears to be invalid. Please double check that you are submitted a valid brewerID.';
+			$this->responseCode = 400;
 			
 			// Log Error
 			$errorLog = new LogError();
@@ -231,17 +239,19 @@ class Brewer {
 					$updateText .= $sqlSetStmt . ', ';
 				}
 				$updateText = substr($updateText, 0, -2);
-				$queryString = "UPDATE brewer SET $updateText, lastModified='$dbLastModified', cbVerified='$dbCBV' WHERE id='$dbBrewerID'"; 
-				$db->query($queryString);
+				$db->query("UPDATE brewer SET $updateText, lastModified='$dbLastModified', cbVerified='$dbCBV' WHERE id='$dbBrewerID'");
 				if($db->error){
 					// Query Error
 					$this->error = true;
 					$this->errorMsg = $db->errorMsg;
+					$this->responseCode = $db->responseCode;
 				}
+				$db->close();
 			}else{
 				// User Validation Error
 				$this->error = true;
 				$this->errorMsg = $users->errorMsg;
+				$this->responseCode = $users->responseCode;
 			}
 		}
 	}
@@ -259,6 +269,7 @@ class Brewer {
 				$this->error = true;
 				$this->validState['name'] = 'invalid';
 				$this->validMsg['name'] = 'We hate to say it but your brewery name is too long for our database. Brewery names are limited to 255 bytes. Any chance you can shorten it?';
+				$this->responseCode = 400;
 				
 				// Log Error
 				$errorLog = new LogError();
@@ -273,13 +284,14 @@ class Brewer {
 			$this->error = true;
 			$this->validState['name'] = 'invalid';
 			$this->validMsg['name'] = 'Please give us the name of the brewery you\'d like to add.';
+			$this->responseCode = 400;
 			
 			// Log Error
 			$errorLog = new LogError();
 			$errorLog->errorNumber = 1;
 			$errorLog->errorMsg = 'Missing brewery name';
 			$errorLog->badData = '';
-			$errorLog->filename = 'API / brewer.class.php';
+			$errorLog->filename = 'API / Brewer.class.php';
 			$errorLog->write();
 		}
 	}
@@ -297,6 +309,7 @@ class Brewer {
 				$this->error = true;
 				$this->validState['description'] = 'invalid';
 				$this->validMsg['description'] = 'We hate to say it but this brewery description is too long for our database. Descriptions are limited to 65,536 bytes. Any chance you can shorten it?';
+				$this->responseCode = 400;
 				
 				// Log Error
 				$errorLog = new LogError();
@@ -322,13 +335,14 @@ class Brewer {
 				$this->error = true;
 				$this->validState['short_description'] = 'invalid';
 				$this->validMsg['short_description'] = 'Sorry, we\'re looking for a short description that is 160 character or less in length. Please shorten the brewery\'s short description to 160 characters or less.';
+				$this->responseCode = 400;
 
 				// Log Error
 				$errorLog = new LogError();
 				$errorLog->errorNumber = 92;
 				$errorLog->errorMsg = 'Short description too long';
 				$errorLog->badData = $this->shortDescription;
-				$errorLog->filename = 'brewer.class.php';
+				$errorLog->filename = 'API / Brewer.class.php';
 				$errorLog->write();
 			}
 		}
@@ -399,13 +413,14 @@ class Brewer {
 						$this->validState[$type] = 'invalid';
 						$this->validMsg[$type] = 'Sorry, something seems to be wrong with your URL. Please check it and try again.';
 						$returnURL = '';
+						$this->responseCode = 400;
 
 						// Log Error
 						$errorLog = new LogError();
 						$errorLog->errorNumber = 107;
 						$errorLog->errorMsg = 'Invalid URL / Failed cURL http';
 						$errorLog->badData = 'URL: ' . $url . ' / HTTP Response Code: ' . $curlResponse['httpCode'];
-						$errorLog->filename = 'brewer.class.php';
+						$errorLog->filename = 'API / Brewer.class.php';
 						$errorLog->write();
 						
 						// Stop Loop
@@ -418,13 +433,14 @@ class Brewer {
 						$this->validState[$type] = 'invalid';
 						$this->validMsg[$type] = 'Sorry, something seems to be wrong with your URL. Please check it and try again.';
 						$returnURL = '';
+						$this->responseCode = 400;
 
 						// Log Error
 						$errorLog = new LogError();
 						$errorLog->errorNumber = 98;
 						$errorLog->errorMsg = 'Too many redirects (+30)';
 						$errorLog->badData = $url;
-						$errorLog->filename = 'brewer.class.php';
+						$errorLog->filename = 'API / Brewer.class.php';
 						$errorLog->write();
 
 						// Stop Loop
@@ -438,19 +454,21 @@ class Brewer {
 					$this->error = true;
 					$this->validStatee[$type] = 'invalid';
 					$this->validMsg[$type] = 'Sorry, but URL strings are limited to 255 bytes in length. Any chance there is a shorter URL you can use?';
+					$this->responseCode = 400;
 				}
 			}else{
 				// Invalid URL
 				$this->error = true;
 				$this->validState[$type] = 'invalid';
 				$this->validMsg[$type] = 'Sorry, something seems to be wrong with your URL. Please check it and try again.';
+				$this->responseCode = 400;
 				
 				// Log Error
 				$errorLog = new LogError();
 				$errorLog->errorNumber = 13;
 				$errorLog->errorMsg = 'Invalid URL';
 				$errorLog->badData = $url;
-				$errorLog->filename = 'brewer.class.php';
+				$errorLog->filename = 'API / Brewer.class.php';
 				$errorLog->write();
 			}
 		}else{
@@ -467,6 +485,7 @@ class Brewer {
 						$this->error = true;
 						$this->validState['facebook_url'] = 'invalid';
 						$this->validMsg['facebook_url'] = 'We were expecting the Facebook URL to start with "https://www.facebook.com/". Please double check the Facebook URL you submitted.';
+						$this->responseCode = 400;
 					}
 					break;
 				case 'twitter_url':
@@ -475,6 +494,7 @@ class Brewer {
 						$this->error = true;
 						$this->validState['twitter_url'] = 'invalid';
 						$this->validMsg['twitter_url'] = 'We were expecting the Twitter URL to start with "https://twitter.com/". Please double check the Twitter URL you submitted.';
+						$this->responseCode = 400;
 					}
 					break;
 				case 'instagram_url':
@@ -483,6 +503,7 @@ class Brewer {
 						$this->error = true;
 						$this->validState['instagram_url'] = 'invalid';
 						$this->validMsg['instagram_url'] = 'We were expecting the Instagram URL to start with "https://www.instagram.com/". Please double check the Instagram URL you submitted.';
+						$this->responseCode = 400;
 					}
 					break;
 			}
@@ -530,7 +551,7 @@ class Brewer {
 			$errorLog->errorNumber = 16;
 			$errorLog->errorMsg = 'cURL Error';
 			$errorLog->badData = "URL: $url / cURL Error: " . curl_error($curl);
-			$errorLog->filename = 'brewer.class.php';
+			$errorLog->filename = 'API / Brewer.class.php';
 			$errorLog->write();
 		}
 		
@@ -595,31 +616,48 @@ class Brewer {
 					// Unexpected number of results
 					$this->error = true;
 					$this->errorMsg = 'Whoops, looks like a bug on our end. We\'ve logged the issue and our support team will look into it.';
+					$this->responseCode = 500;
 
 					// Log Error
 					$errorLog = new LogError();
 					$errorLog->errorNumber = 19;
 					$errorLog->errorMsg = 'Unexpected number of results';
 					$errorLog->badData = "brewerID: $brewerID";
-					$errorLog->filename = 'brewer.class.php';
+					$errorLog->filename = 'API / Brewer.class.php';
+					$errorLog->write();
+				}else{
+					// Brewer Does Not Exist
+					$this->error = true;
+					$this->errorMsg = "Sorry, we couldn't find a brewer with the brewerID you provided.";
+					$this->responseCode = 400;
+					
+					// Log Error
+					$errorLog = new LogError();
+					$errorLog->errorNumber = 133;
+					$errorLog->errorMsg = 'brewerID Not Found';
+					$errorLog->badData = "brewerID: $brewerID";
+					$errorLog->filename = 'API / Brewer.class.php';
 					$errorLog->write();
 				}
 			}else{
 				// Query Error
 				$this->error = true;
 				$this->errorMsg = $db->errorMsg;
+				$this->responseCode = $db->responseCode;
 			}
+			$db->close();
 		}else{
 			// Missing BrewerID
 			$this->error = true;
 			$this->errorMsg = 'Whoops, we seem to be missing the brewer_id for the brewer. Please check your request and try again.';
+			$this->responseCode = 400;
 			
 			// Log Error
 			$errorLog = new LogError();
 			$errorLog->errorNumber = 18;
 			$errorLog->errorMsg = 'Missing brewer ID';
 			$errorLog->badData = '';
-			$errorLog->filename = 'brewer.class.php';
+			$errorLog->filename = 'API / Brewer.class.php';
 			$errorLog->write();
 		}
 								
@@ -644,6 +682,7 @@ class Brewer {
 					// Outside Range
 					$this->error = true;
 					$this->errorMsg = 'Sorry, the cursor value you supplied is outside our data range.';
+					$this->responseCode = 400;
 
 					// Log Error
 					$errorLog = new LogError();
@@ -658,6 +697,7 @@ class Brewer {
 					// Outside Range
 					$this->error = true;
 					$this->errorMsg = 'Sorry, the count value you specified is outside our acceptable range. The range we will accept is [1, 1,000,000].';
+					$this->responseCode = 400;
 
 					// Log Error
 					$errorLog = new LogError();
@@ -671,6 +711,7 @@ class Brewer {
 				// Not an integer offset
 				$this->error = true;
 				$this->errorMsg = 'Sorry, the count value you supplied is invalid. Please ensure you are sending an integer value.';
+				$this->responseCode = 400;
 
 				// Log Error
 				$errorLog = new LogError();
@@ -684,6 +725,7 @@ class Brewer {
 			// Not an integer offset
 			$this->error = true;
 			$this->errorMsg = 'Sorry, the cursor value you supplied is invalid.';
+			$this->responseCode = 400;
 			
 			// Log Error
 			$errorLog = new LogError();
@@ -707,7 +749,9 @@ class Brewer {
 				// Query Error
 				$this->error = true;
 				$this->errorMsg = $db->errorMsg;
+				$this->responseCode = $db->responseCode;
 			}
+			$db->close();
 		}
 		
 		// Return
@@ -745,7 +789,9 @@ class Brewer {
 			// Query Error
 			$this->error = true;
 			$this->errorMsg = $db->errorMsg;
+			$this->responseCode = $db->responseCode;
 		}
+		$db->close();
 		
 		return $count;
 	}
@@ -759,6 +805,7 @@ class Brewer {
 				$dbLastModified = $db->escape(time());
 				$dbBrewerID = $db->escape($brewerID);
 				$db->query("UPDATE brewer SET lastModified='$dbLastModified' WHERE id='$dbBrewerID'");
+				$db->close();
 			}else{
 				// Invalid brewerID
 				// Log Error
@@ -795,7 +842,9 @@ class Brewer {
 			// Query Error
 			$this->error = true;
 			$this->errorMsg = $db->errorMsg;
+			$this->responseCode = $db->responseCode;
 		}
+		$db->close();
 		
 		// Return
 		return $lastModified;
@@ -808,23 +857,12 @@ class Brewer {
 		if(!empty($brewerID)){
 			if($this->validate($brewerID, true)){
 				$lastModified = $this->lastModified;
-			}else{
-				// Invalid Brewer
-				$this->error = true;
-				$this->errorMsg = 'Missing brewerID';
-
-				// Log Error
-				$errorLog = new LogError();
-				$errorLog->errorNumber = 103;
-				$errorLog->errorMsg = 'Invalid brewerID';
-				$errorLog->badData = $brewerID;
-				$errorLog->filename = 'API / Brewer.class.php';
-				$errorLog->write();
 			}
 		}else{
 			// Missing BrewerID
 			$this->error = true;
 			$this->errorMsg = 'Missing brewerID';
+			$this->responseCode = 400;
 			
 			// Log Error
 			$errorLog = new LogError();
