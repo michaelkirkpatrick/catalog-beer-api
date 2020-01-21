76163e616e728bda500c27ac3207a8314e996783
Michael Kirkpatrick <michael@mekstudios.com>
2020-01-20 19:54:07 -0800

    Updated SendEmail.class.php - Testing Function

--- classes/SendEmail.class.php
+++ classes/SendEmail.class.php
@@ -10,6 +10,7 @@ class SendEmail {
 	// Variables
 	public $email;
 	private $postmarkServerToken = '';
+	private $testing = true;
 	
 	// Validation
 	public $error = false;
@@ -87,77 +88,79 @@ class SendEmail {
 			$uuid = new uuid();
 			$authCode = $uuid->createCode();
 
-			// Email Basics
-			$to = $email;
-			$subject = 'Confirm your Catalog.beer Account';
-			$tag = 'confirm-email';
+			if(!$this->testing){
+				// Email Basics
+				$to = $email;
+				$subject = 'Confirm your Catalog.beer Account';
+				$tag = 'confirm-email';
 
-			// Plain Text
-			$textBody = "Hello!\r\n\r\nWelcome to the Catalog.beer community. We'd like to confirm your email address (as a proxy for you being a real human being and not a computer). If you don't mind, please click on the link below to confirm your account.\r\n\r\nhttps://catalog.beer/verify-email/$authCode\r\n\r\nOnce you've confirmed your account, you'll be able to contribute to the Catalog.beer database.\r\n\r\nThanks!\r\n\r\n-Michael\r\n\r\nMichael Kirkpatrick\r\nFounder, Catalog.beer\r\nmichael@catalog.beer";
+				// Plain Text
+				$textBody = "Hello!\r\n\r\nWelcome to the Catalog.beer community. We'd like to confirm your email address (as a proxy for you being a real human being and not a computer). If you don't mind, please click on the link below to confirm your account.\r\n\r\nhttps://catalog.beer/verify-email/$authCode\r\n\r\nOnce you've confirmed your account, you'll be able to contribute to the Catalog.beer database.\r\n\r\nThanks!\r\n\r\n-Michael\r\n\r\nMichael Kirkpatrick\r\nFounder, Catalog.beer\r\nmichael@catalog.beer";
 
-			// HTML Email
-			$htmlBody = file_get_contents(ROOT . '/classes/resources/email-head.html');
-			$htmlBody .= file_get_contents(ROOT . '/classes/resources/email-confirm-address.html');
-			$htmlBody = str_replace('##AUTHCODE##', $authCode, $htmlBody);
+				// HTML Email
+				$htmlBody = file_get_contents(ROOT . '/classes/resources/email-head.html');
+				$htmlBody .= file_get_contents(ROOT . '/classes/resources/email-confirm-address.html');
+				$htmlBody = str_replace('##AUTHCODE##', $authCode, $htmlBody);
 
-			$postmarkSendEmail = new PostmarkSendEmail();
-			$postmarkSendEmail->generateBody($to, $subject, $tag, $htmlBody, $textBody);
+				$postmarkSendEmail = new PostmarkSendEmail();
+				$postmarkSendEmail->generateBody($to, $subject, $tag, $htmlBody, $textBody);
 
-			$json = json_encode($postmarkSendEmail);
+				$json = json_encode($postmarkSendEmail);
 
-			// Start cURL
-			$curl = curl_init();
+				// Start cURL
+				$curl = curl_init();
 
-			curl_setopt_array($curl, array(
-				CURLOPT_URL => "https://api.postmarkapp.com/email",
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_MAXREDIRS => 10,
-				CURLOPT_TIMEOUT => 30,
-				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-				CURLOPT_CUSTOMREQUEST => "POST",
-				CURLOPT_HTTPHEADER => array(
-					"Accept: application/json",
-					"cache-control: no-cache",
-					"Content-Type: application/json",
-					"X-Postmark-Server-Token: " . $this->postmarkServerToken
-				),
-				CURLOPT_POSTFIELDS => "$json"
-			));
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => "https://api.postmarkapp.com/email",
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 30,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => "POST",
+					CURLOPT_HTTPHEADER => array(
+						"Accept: application/json",
+						"cache-control: no-cache",
+						"Content-Type: application/json",
+						"X-Postmark-Server-Token: " . $this->postmarkServerToken
+					),
+					CURLOPT_POSTFIELDS => "$json"
+				));
 
-			$response = curl_exec($curl);
-			$err = curl_error($curl);
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
 
-			curl_close($curl);
+				curl_close($curl);
 
-			if($err){
-				// cURL Error
-				$this->error = true;
-				$this->errorMsg = 'Whoops, looks like a bug on our end. We\'ve logged the issue and our support team will look into it.';
-				$this->responseCode = 500;
-
-				// Log Error
-				$errorLog = new LogError();
-				$errorLog->errorNumber = 128;
-				$errorLog->errorMsg = 'cURL Error';
-				$errorLog->badData = $err;
-				$errorLog->filename = 'API / SendEmail.class.php';
-				$errorLog->write();
-			}else{
-				// Response Received
-				$decodedReponse = json_decode($response);
-				if($decodedReponse->ErrorCode != 0){
-					// Error Sending Email
+				if($err){
+					// cURL Error
 					$this->error = true;
-					$this->errorMsg = 'Sorry, there was an error sending your confirmation email. We\'ve logged the issue and our support team will look into it.';
+					$this->errorMsg = 'Whoops, looks like a bug on our end. We\'ve logged the issue and our support team will look into it.';
 					$this->responseCode = 500;
 
 					// Log Error
 					$errorLog = new LogError();
-					$errorLog->errorNumber = 129;
-					$errorLog->errorMsg = 'Postmark App Error';
-					$errorLog->badData = $decodedReponse;
+					$errorLog->errorNumber = 128;
+					$errorLog->errorMsg = 'cURL Error';
+					$errorLog->badData = $err;
 					$errorLog->filename = 'API / SendEmail.class.php';
 					$errorLog->write();
+				}else{
+					// Response Received
+					$decodedReponse = json_decode($response);
+					if($decodedReponse->ErrorCode != 0){
+						// Error Sending Email
+						$this->error = true;
+						$this->errorMsg = 'Sorry, there was an error sending your confirmation email. We\'ve logged the issue and our support team will look into it.';
+						$this->responseCode = 500;
+
+						// Log Error
+						$errorLog = new LogError();
+						$errorLog->errorNumber = 129;
+						$errorLog->errorMsg = 'Postmark App Error';
+						$errorLog->badData = $decodedReponse;
+						$errorLog->filename = 'API / SendEmail.class.php';
+						$errorLog->write();
+					}
 				}
 			}
 
